4a5343352072a2110b07091060cc2f885784534e
Alam Rincon <alamrdrgz98@gmail.com>
2026-01-15 22:03:29 +0000

    Small Fix

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/YOUR_REPO_NAME/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/SlotScholars_Site/' : '/',
 })
